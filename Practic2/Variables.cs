fa998cf0e9bd7cd07efc4331a92157066ddf1933
Vitalii Viktorovich <Vitalii Viktorovich@DESKTOP-ETUL9PS>
2022-09-11 13:19:46 +0300

    Sorted project structure

---
+++ Practic2/Variables.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic2
+{
+    internal class Variables
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+            Console.ReadLine();
+        }
+    }
+}
